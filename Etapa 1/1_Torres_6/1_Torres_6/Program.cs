a2ae3e98de73407396037ab8bf1de8029d414e48
TRTorres <thiagortorres8@gmail.com>
2025-09-28 14:13:57 -0300

    Add files via upload

---
+++ Etapa 1/1_Torres_6/1_Torres_6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1_Torres_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperatura;
+            Console.WriteLine("Ingrese la temperatura del agua");
+            temperatura = Convert.ToInt32(Console.ReadLine());
+            if (temperatura <= 0)
+            {
+                Console.WriteLine("El agua se congela");
+            }
+            else if (temperatura >= 100)
+            {
+                Console.WriteLine("El agua se hierve");
+            }
+            else
+            {
+                Console.WriteLine("El agua esta en estado liquido");
+            }
+        }
+    }
+}

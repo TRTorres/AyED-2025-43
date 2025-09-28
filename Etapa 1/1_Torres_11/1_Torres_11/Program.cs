ae2de5177d8d47c76a5d156c55400c9565c4c3c7
TRTorres <thiagortorres8@gmail.com>
2025-09-28 15:51:24 -0300

    Add files via upload

---
+++ Etapa 1/1_Torres_11/1_Torres_11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1_Torres_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Escriba un numero");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < (num+1); i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}

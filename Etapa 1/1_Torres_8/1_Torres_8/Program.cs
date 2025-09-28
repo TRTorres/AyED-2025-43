35438d78ed9d89f48d43ce3e7f33e3a8603523cb
TRTorres <thiagortorres8@gmail.com>
2025-09-28 14:23:29 -0300

    Add files via upload

---
+++ Etapa 1/1_Torres_8/1_Torres_8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1_Torres_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad, ayu;
+            Console.WriteLine("Cual es su edad");
+            edad = Convert.ToInt32(Console.ReadLine());
+            ayu = 0;
+            for (int i = 0; i < edad; i++)
+            {
+                ayu = ayu + 1;
+                Console.WriteLine("Usted cumplio " + ayu + " años");
+            }
+        }
+    }
+}

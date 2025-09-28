723cd19ecb6c0e85ddf1e067693f167ed4fe53c0
TRTorres <thiagortorres8@gmail.com>
2025-09-28 20:38:40 -0300

    Add files via upload

---
+++ Etapa 2/2_Torres_6/2_Torres_6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Cantidad de candidatos: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] puntajes = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Puntaje del candidato " + (i+1) +":");
+            puntajes[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Array.Sort(puntajes); 
+
+        Console.Write("Número para filtrar múltiplos: ");
+        int filtro = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Puntajes ordenados:");
+        for (int i = 0; i < n; i++)
+            Console.WriteLine(puntajes[i]);
+
+
+        Console.WriteLine("Puntajes múltiplos de " + filtro + ":");
+        for (int i = 0; i < n; i++)
+            if (puntajes[i] % filtro == 0)
+                Console.WriteLine(puntajes[i]);
+
+    }
+}

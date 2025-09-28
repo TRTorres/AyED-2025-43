ea7a80d860968314f19fd598c18213b5190217f3
TRTorres <thiagortorres8@gmail.com>
2025-09-28 20:21:28 -0300

    Add files via upload

---
+++ Etapa 2/2_Torres_4/2_Torres_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Cantidad de productos: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int max = int.MinValue;
+        int min = int.MaxValue;
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("Precio del producto " + (i+1) + " :");
+            int precio = int.Parse(Console.ReadLine());
+
+            if (precio > max) max = precio;
+            if (precio < min) min = precio;
+        }
+
+        Console.WriteLine("Más caro: $" + max);
+        Console.WriteLine("Más barato: $" + min);
+    }
+}

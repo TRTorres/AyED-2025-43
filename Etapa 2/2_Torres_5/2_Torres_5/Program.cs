ba5d0d11bf5e366d11556724f65212fac398e2ff
TRTorres <thiagortorres8@gmail.com>
2025-09-28 20:33:55 -0300

    Add files via upload

---
+++ Etapa 2/2_Torres_5/2_Torres_5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Cantidad de objetos: ");
+        int cantidad = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Valor mínimo para filtrar: ");
+        int minimo = Convert.ToInt32(Console.ReadLine());
+
+        int[] valores = new int[cantidad];
+
+        for (int i = 0; i < cantidad; i++)
+        {
+            Console.Write("Valor del objeto " + (i+1) + ":");
+            valores[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("Posiciones con valor mayor al mínimo:");
+        for (int i = 0; i < cantidad; i++)
+        {
+            if (valores[i] > minimo)
+                Console.WriteLine("Posición " + (i + 1) + " Valor: " + valores[i]);
+        }
+    }
+}

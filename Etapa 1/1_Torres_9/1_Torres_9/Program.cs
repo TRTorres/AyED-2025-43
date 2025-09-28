47e16b6b8f67856d33a24e39c9c3371a57b3c486
TRTorres <thiagortorres8@gmail.com>
2025-09-28 15:45:01 -0300

    Add files via upload

---
+++ Etapa 1/1_Torres_9/1_Torres_9/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1_Torres_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num_usu;
+            Console.WriteLine("Escriba un numero entero positivo");
+            num_usu = Convert.ToInt32(Console.ReadLine());
+            for (int i = num_usu; i > 0; i--)
+            {
+                Console.WriteLine("El numero es " + i);
+            }
+            Console.WriteLine("El numero es " + 0);
+        }
+    }
+}

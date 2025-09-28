d8afe13e9454845c34f2a3dd6acef10a62f1c1bc
TRTorres <thiagortorres8@gmail.com>
2025-09-28 15:56:25 -0300

    Add files via upload

---
+++ Etapa 1/1_Torres_12/1_Torres_12/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string respuesta;
+
+        do
+        {
+            Console.Write("¿Desea terminar el programa? ");
+            respuesta = Console.ReadLine();
+        }
+        while (respuesta != "SI");
+
+        Console.WriteLine("Programa finalizado.");
+    }
+}

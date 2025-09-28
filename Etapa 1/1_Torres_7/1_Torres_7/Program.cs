52ff08e7ae130856ef1992ad0f041cffe87ad80b
TRTorres <thiagortorres8@gmail.com>
2025-09-28 14:17:51 -0300

    Add files via upload

---
+++ Etapa 1/1_Torres_7/1_Torres_7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1_Torres_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pal;
+            Console.WriteLine("Escriba una palabra");
+            pal = Console.ReadLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(pal);
+            }
+        }
+    }
+}

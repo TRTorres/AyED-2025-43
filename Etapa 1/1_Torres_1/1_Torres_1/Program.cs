ddc459cd9d0124f1086657dfff243f69d4030446
TRTorres <thiagortorres8@gmail.com>
2025-09-28 13:09:45 -0300

    Add files via upload

---
+++ Etapa 1/1_Torres_1/1_Torres_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string contraseña = "MiSecreta123";
+
+        Console.Write("Introduce la contraseña: ");
+        string entrada = Console.ReadLine();
+
+        if (contraseña.ToLower() == entrada.ToLower())
+        {
+            Console.WriteLine("✅ Contraseña correcta.");
+        }
+        else
+        {
+            Console.WriteLine("❌ Contraseña incorrecta.");
+        }
+    }
+}

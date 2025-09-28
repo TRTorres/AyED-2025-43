64db7afbd0d4a978a94498ac33d04e2fc13cb4c0
TRTorres <thiagortorres8@gmail.com>
2025-09-28 16:01:25 -0300

    Add files via upload

---
+++ Etapa 1/1_Torres_13/1_Torres_13/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class algo
+{
+    static void Main()
+    {
+        int opcion = 0;
+
+        Console.WriteLine("Bienvenido al programa!");
+
+        while (opcion != 4)
+        {
+            Console.WriteLine("\n--- Menú ---");
+            Console.WriteLine("1. Saludo");
+            Console.WriteLine("2. Sumar 2 + 2");
+            Console.WriteLine("3. Mostrar la hora");
+            Console.WriteLine("4. Salir");
+            Console.Write("Elija una opción: ");
+            opcion = Convert.ToInt32(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("¡Hola, Usuario!");
+                    break;
+
+                case 2:
+                    Console.WriteLine("2 + 2 = 4");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Hora actual: " + DateTime.Now);
+                    break;
+
+                case 4:
+                    Console.WriteLine("Adiós!");
+                    break;
+            }
+        }
+    }
+}

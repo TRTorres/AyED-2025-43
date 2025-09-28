792355863ee5515c62c6b5b763a9d7eeabedadef
TRTorres <thiagortorres8@gmail.com>
2025-09-28 14:06:11 -0300

    Add files via upload

---
+++ Etapa 1/1_Torres_5/1_Torres_5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1_Torres_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string comida, refugio, bate;
+            Console.WriteLine("¿Tiene comida?");
+            comida = Console.ReadLine().ToLower();
+            Console.WriteLine("¿Tiene un refugio?");
+            refugio = Console.ReadLine().ToLower();
+            Console.WriteLine("¿Tiene un bate?");
+            bate = Console.ReadLine().ToLower();
+            if (comida == "si" && refugio == "si" && bate == "si")
+            {
+                Console.WriteLine("Puede sobrevivir");
+            }
+            else
+            {
+                Console.WriteLine("No puede sobrevivir");
+            }
+
+        }
+    }
+}

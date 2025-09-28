7282ffc19e069582802b03cfe5ac1618a3826391
TRTorres <thiagortorres8@gmail.com>
2025-09-28 13:47:32 -0300

    Add files via upload

---
+++ Etapa 1/1_Torres_3/1_Torres_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1_Torres_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num_usu;
+            Console.WriteLine("Escriba un numero");
+            num_usu = Convert.ToInt32(Console.ReadLine());
+            if (num_usu == 0)
+            {
+                Console.WriteLine("No se puede dividir un 0");
+            }
+            else if (num_usu % 2 == 0)
+            {
+                Console.WriteLine("El numero es par");
+            }
+            else
+            {
+                Console.WriteLine("El numero es impar");
+            }
+        }
+    }
+}

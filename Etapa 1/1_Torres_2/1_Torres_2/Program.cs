9450e162d9b5924b84696b00da9ae5a6f5302d2f
TRTorres <thiagortorres8@gmail.com>
2025-09-28 13:38:41 -0300

    Add files via upload

---
+++ Etapa 1/1_Torres_2/1_Torres_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1_Torres_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero_usu, num_usu, division;
+            Console.WriteLine("Escribe un numero");
+            numero_usu = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe otro numero");
+            num_usu = Convert.ToInt32(Console.ReadLine()); 
+            if (num_usu == 0)
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            { 
+                division = 0;
+                division = numero_usu / num_usu;
+                Console.WriteLine("El resusltado de la division es: " + division);
+            }
+        }
+    }
+}

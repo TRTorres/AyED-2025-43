c0c2db7e596a7eb0b085e7213b52b6f236f7829c
TRTorres <thiagortorres8@gmail.com>
2025-09-28 12:57:36 -0300

    Add files via upload

---
+++ Etapa 1/1_Torres_0/1_Torres_0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1_Torres_0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad_usua;
+            Console.WriteLine("Cuantos años tenes?");
+            edad_usua = Convert.ToInt32(Console.ReadLine());
+            if (edad_usua >= 18)
+            {
+                Console.WriteLine("Sos mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Sos menor de edad");
+            }
+        }
+    }
+}

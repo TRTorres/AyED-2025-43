e8ecf3c9e6c6ddcde52161669fb05f099d738ac1
TRTorres <thiagortorres8@gmail.com>
2025-09-28 13:58:45 -0300

    Add files via upload

---
+++ Etapa 1/1_Torres_4/1_Torres_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _1_Torres_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad, ingresos;
+            Console.WriteLine("Escriba su edad");
+            edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escriba sus ingresos mensuales");
+            ingresos = Convert.ToInt32(Console.ReadLine());
+            if (edad > 19 && ingresos <= 100000)
+            {
+                Console.WriteLine("Puede cobrar la beca");
+            }
+            else if (edad < 19 && ingresos == 0)
+            {
+                Console.WriteLine("Puede cobrar la beca");
+            }
+            else if (edad == 19 && ingresos <= 50000)
+            {
+                Console.WriteLine("Puede cobrar la beca");
+            }
+        }
+    }
+}

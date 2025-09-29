c96cdc952671258ed5d8aeb7765ddad28cf10072
TRTorres <thiagortorres8@gmail.com>
2025-09-28 21:16:37 -0300

    Add files via upload

---
+++ Etapa 3/3_Torres_0/3_Torres_0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3_Torres_0
+{
+    internal class Program
+    {
+        static void nombre()
+        {
+            string nombre;
+            Console.WriteLine("Escribe tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+        } 
+        static void Main(string[] args)
+        {
+            nombre();
+        }
+    }
+}

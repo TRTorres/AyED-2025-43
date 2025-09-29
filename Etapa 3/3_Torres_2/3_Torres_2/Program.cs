6ced225cafb42ab5fdc87344649a9db656bf38ce
TRTorres <thiagortorres8@gmail.com>
2025-09-28 21:23:41 -0300

    Add files via upload

---
+++ Etapa 3/3_Torres_2/3_Torres_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CalculadoraSimple
+{
+    internal class Program
+    {
+        static int Sumar(int a, int b) => a + b;
+        static int Restar(int a, int b) => a - b;
+        static int Multiplicar(int a, int b) => a * b;
+        static int Dividir(int a, int b) => a / b;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Número 1:");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Número 2:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Operación (+ - * /):");
+            string op = Console.ReadLine();
+
+            switch (op)
+            {
+                case "+":
+                    Console.WriteLine(Sumar(x, y));
+                    break;
+                case "-":
+                    Console.WriteLine(Restar(x, y));
+                    break;
+                case "*":
+                    Console.WriteLine(Multiplicar(x, y));
+                    break;
+                case "/":
+                    Console.WriteLine(Dividir(x, y));
+                    break;
+                default:
+                    Console.WriteLine("Operación inválida");
+                    break;
+            }
+        }
+    }
+}

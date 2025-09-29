80f5afebb2f89a74f98adf1e5537a837cf38b53c
TRTorres <thiagortorres8@gmail.com>
2025-09-28 21:06:18 -0300

    Add files via upload

---
+++ Etapa 2/2_Torres_13/2_Torres_13/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Cantidad de estudiantes: ");
+        int n = int.Parse(Console.ReadLine());
+
+        string[,] estudiantes = new string[n, 3];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Estudiante " + (i + 1));
+
+            Console.Write("Nombre: ");
+            estudiantes[i, 0] = Console.ReadLine();
+
+            Console.Write("Edad: ");
+            estudiantes[i, 1] = Console.ReadLine();
+
+            Console.Write("Calificación: ");
+            estudiantes[i, 2] = Console.ReadLine();
+        }
+
+        Console.WriteLine("Listado de estudiantes:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Nombre: " + estudiantes[i, 0] +
+                              ", Edad: " + estudiantes[i, 1] +
+                              ", Calificación: " + estudiantes[i, 2]);
+        }
+    }
+}

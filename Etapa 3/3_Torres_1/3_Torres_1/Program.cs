134d67b9f54a6617977b66ff0e6dbbeb3ae4fee5
TRTorres <thiagortorres8@gmail.com>
2025-09-28 21:21:30 -0300

    Add files via upload

---
+++ Etapa 3/3_Torres_1/3_Torres_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3_Torres_1
+{
+    internal class Program
+    {
+        static int PedirNumero()
+        {
+            Console.WriteLine("Escriba un número:");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            int num1 = PedirNumero();
+            int num2 = PedirNumero();
+            int suma = num1 + num2;
+
+            Console.WriteLine("La suma de los dos números es: " + suma);
+        }
+    }
+}

6b69d80f5f27f2351f0bf7b77146c88ca9a18a4c
TRTorres <thiagortorres8@gmail.com>
2025-09-28 21:00:52 -0300

    Add files via upload

---
+++ Etapa 2/2_Torres_12/2_Torres_12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el tamaño n de la matriz: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[,] matriz = new int[n, n];
+        int[] diagonal = new int[n];
+        Random r = new Random();
+
+        Console.WriteLine("Matriz generada:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matriz[i, j] = r.Next(1, 100);
+                Console.Write(matriz[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        for (int i = 0; i < n; i++)
+            diagonal[i] = matriz[i, i];
+
+        Console.WriteLine("Diagonal principal:");
+        for (int i = 0; i < n; i++)
+            Console.WriteLine("Posición " + i + ": " + diagonal[i]);
+    }
+}
